18ff9bdd3e9678e9a7a15d652ba4bbc7d86da544
comp0717 <82495195+Raxxer44@users.noreply.github.com>
2022-11-10 17:11:25 +0300

    Create Massiv

---
+++ myArray/myArray/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+class SergArray
+{
+    static void Main(string[] args)
+    {
+        int[] myArray = new int[10];
+        Random rand = new Random();
+
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            myArray[i] = rand.Next(33);
+        }
+        Console.WriteLine("myArray: ");
+
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            Console.WriteLine("Значение элемента массива (индекс " + i + ") = " + myArray[i]);
+        }
+        Console.WriteLine();
+
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            if (myArray[i] % 3 == 0)
+            {
+                Console.WriteLine("Значение элемента массива делённого на 3 (индекс " + i + ") = " + myArray[i]);
+            }
+        }
+        Console.WriteLine();
+
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            if (myArray[i] % 5 == 0)
+            {
+                Console.WriteLine("Значение элемента массива делённого на 5 (индекс " + i + ") = " + myArray[i]);
+            }
+        }
+        Console.WriteLine();
+
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            if ((myArray[i] % 3 == 0) && (myArray[i] % 5 == 0))
+            {
+                Console.WriteLine("Значение элемента массива делённого на 3 и 5 (индекс " + i + ") = " + myArray[i]);
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
